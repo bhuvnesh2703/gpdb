ab2e41f3d76367b6eb8a60a0f76f36895d93cf95
Fenggang <ginobiliwang@gmail.com>
2017-09-14 03:13:45 -0400

    test with ci

--- src/test/regress/sql/gp_dump_query_oids.sql
+++ src/test/regress/sql/gp_dump_query_oids.sql
@@ -15,3 +15,19 @@ SELECT gp_dump_query_oids('SELECT with syntax error');
 
 -- Test partition and inherited tables
 CREATE TABLE minirepro_partition_test (id int, info json);
+CREATE TABLE foo (id int, year int, a int, b int, c int, d int, region text)
+ DISTRIBUTED BY (id)
+ PARTITION BY RANGE (year)
+     SUBPARTITION BY RANGE (a)
+        SUBPARTITION TEMPLATE (
+         START (1) END (2) EVERY (1),
+         DEFAULT SUBPARTITION other_a )
+            SUBPARTITION BY RANGE (b)
+               SUBPARTITION TEMPLATE (
+               START (1) END (2) EVERY (1),
+               DEFAULT SUBPARTITION other_b )
+( START (2002) END (2003) EVERY (1),
+   DEFAULT PARTITION outlying_years );
+CREATE TABLE ptable (c1 text, c2 float);
+CREATE TABLE ctable (c3 char(2)) INHERITS (ptable);
+CREATE TABLE cctable (c4 char(2)) INHERITS (ctable);
