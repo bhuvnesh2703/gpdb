6dbb0b1b26a95b6189db05c77032490f3745209b
Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
2018-04-04 11:00:27 -0700

    Updated to use gpdb

--- concourse/scripts/untar_and_build_gpdb.py
+++ concourse/scripts/untar_and_build_gpdb.py
@@ -2,5 +2,5 @@
 import subprocess
 
 subprocess.call(["mkdir -p gpdb_src && tar -xf gpdb_tarball/gpdb_src.tar.gz -C gpdb_src --strip 1"], shell=True)
-subprocess.call(["gpdb_src/concourse/scripts/build_gpdb.py --mode=orca --output_dir=bin_gpdb/install --action=build --configure-option=--disable-gpcloud --orca-in-gpdb-install-location bin_orca bin_xerces"], shell=True)
-subprocess.call(["env src_root=bin_gpdb/install dst_tarball=package_tarball/bin_gpdb.tar.gz gpdb_src/concourse/scripts/package_tarball.bash"], shell=True)
+subprocess.call(["gpdb/concourse/scripts/build_gpdb.py --mode=orca --output_dir=bin_gpdb/install --action=build --configure-option=--disable-gpcloud --orca-in-gpdb-install-location bin_orca bin_xerces"], shell=True)
+subprocess.call(["env src_root=bin_gpdb/install dst_tarball=package_tarball/bin_gpdb.tar.gz gpdb/concourse/scripts/package_tarball.bash"], shell=True)
