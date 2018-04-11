2312ae180785ba31baae2a54472f642801187767
Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
2018-04-10 22:47:55 -0700

    test

--- concourse/scripts/untar_and_build_gpdb.py
+++ concourse/scripts/untar_and_build_gpdb.py
@@ -16,6 +16,7 @@ def exec_command(cmd):
 untar_gpdb_cmd = "mkdir -p gpdb_src && tar -xf gpdb_tarball/gpdb_src.tar.gz -C gpdb_src --strip 1"
 exec_command(untar_gpdb_cmd)
 
+print "Beginning to {0}".format(action)
 if action == 'build':
     build_gpdb_cmd = "gpdb_src/concourse/scripts/build_gpdb.py --mode={0} --output_dir=bin_gpdb/install --action={1} --configure-option={2} --orca-in-gpdb-install-location bin_orca bin_xerces".format(mode, action, configure_option)
     exec_command(build_gpdb_cmd)
