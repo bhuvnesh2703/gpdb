13b3e6a0334e90b3d5ba54c21235bdd442105d4d
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2020-10-14 11:41:40 +0300

    Remove es_result_relation_info from EState.
    Maintaining 'es_result_relation_info' correctly at all times has become
    cumbersome, especially with partitioning where each partition gets its
    own result relation info. Having to set and reset it across arbitrary
    operations has caused bugs in the past.
    
    This changes all the places that used 'es_result_relation_info', to
    receive the currently active ResultRelInfo via function parameters
    instead.
    
    Author: Amit Langote
    Discussion: https://www.postgresql.org/message-id/CA%2BHiwqGEmiib8FLiHMhKB%2BCH5dRgHSLc5N5wnvc4kym%2BZYpQEQ%40mail.gmail.com
    (cherry picked from commit a04daa97a4339c38e304cd6164d37da540d665a8)

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -2148,7 +2148,6 @@ ExecuteTruncateGuts(List *explicit_rels, List *relids, List *relids_logged,
 	resultRelInfo = resultRelInfos;
 	foreach(cell, rels)
 	{
-		estate->es_result_relation_info = resultRelInfo;
 		ExecBSTruncateTriggers(estate, resultRelInfo);
 		resultRelInfo++;
 	}
@@ -2312,7 +2311,6 @@ ExecuteTruncateGuts(List *explicit_rels, List *relids, List *relids_logged,
 	resultRelInfo = resultRelInfos;
 	foreach(cell, rels)
 	{
-		estate->es_result_relation_info = resultRelInfo;
 		ExecASTruncateTriggers(estate, resultRelInfo);
 		resultRelInfo++;
 	}
